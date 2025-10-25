64a60edef045d62018ab2b16300cea2dadab9951
Walby12 <alby09.bonf@gmail.com>
2025-10-25 18:10:52 +0200

    Made the lexer

--- src/lexer.c
+++ src/lexer.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
+char lex_id[1024];
+int lex_num;
 int cur_line = 1;
 int cur_col = 0;
 
@@ -14,7 +18,6 @@ Lexer* lexer_init(const char *cont) {
 	}
 	lex->cont = cont;
 	lex->index = 0;
-	lex->cur_string = NULL;
 	return lex;
 }
 
@@ -34,7 +37,63 @@ void get_next_tok(Lexer *lex) {
 		case '\0':
 			lex->cur_tok = STR_END;
 			break;
+		case '(':
+			lex->cur_tok = OPEN_PAREN;
+			cur_col++;
+			break;
+		case ')':
+			lex->cur_tok = CLOSE_PAREN;
+			cur_col++;
+			break;
+		case ';':
+			lex->cur_tok = SEMICOLON;
+			cur_col++;
+			break;
+		case '{':
+			lex->cur_tok = OPEN_CURLY;
+			cur_col++;
+			break;
+		case '}':
+			lex->cur_tok = CLOSE_CURLY;
+			cur_col++;
+			break;
+		case '-':
+			if (lex->cont[lex->index] != '>') {
+				printf("ERROR [%d,%d]: unknow token: %c\n", cur_col, cur_line, c);
+				exit(1);
+			} else {
+				lex->cur_tok = ARROW;
+				cur_col++;
+				lex->index++;
+				break;
+			}
 		default:
-			printf("%c\n", c);
+			if (isalpha(c)) {
+				char builder[1024];
+				int i = 0;
+				while (isalpha(c) && c != '\0' && i < (int) sizeof(builder) - 1) {
+					builder[i++] = c;
+					c = lex->cont[lex->index++];
+					cur_col++;
+				}
+				builder[i] = '\0';
+				memcpy(lex_id, builder, i);
+				lex_id[i] = '\0';
+				lex->cur_tok = ID;
+			} else if (isdigit(c)) {
+				char builder[1024];
+				int i = 0;
+				while (isdigit(c) && c != '\0' && i < (int) sizeof(builder) - 1) {
+					builder[i++] = c;
+					c = lex->cont[lex->index++];
+					cur_col++;
+				}
+				builder[i] = '\0';
+				lex_num = atoi(builder);
+				lex->cur_tok = NUM;
+			} else {
+				printf("ERROR [%d,%d]: unknow token: %c\n", cur_col, cur_line, c);
+				exit(1);
+			}
 	}
 }
--- src/lexer.h
+++ src/lexer.h
@@ -1,11 +1,25 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+extern char lex_id[1024];
+extern int lex_num;
+extern int cur_line;
+extern int cur_col;
+
 typedef enum {
 	ID,
+	NUM,
+	OPEN_PAREN,
+	CLOSE_PAREN,
+	SEMICOLON,
+	OPEN_CURLY,
+	CLOSE_CURLY,
+	ARROW,
 	STR_END
 } TokenType;
 
 typedef struct {
 	const char *cont;
-	char *cur_string;
 	int index;
 	TokenType cur_tok;
 } Lexer;
@@ -13,3 +27,5 @@ typedef struct {
 Lexer* lexer_init(const char *cont);
 
 void get_next_tok(Lexer *lex);
+
+#endif
